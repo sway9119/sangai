10c3cb29cd67c8dd081b069f55dc6707cc6a3e0f
sway <40759948+sway9119@users.noreply.github.com>
2021-05-01 21:42:26 +0900

    Add CollectionsControllser

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('collections', 'App\Http\Controllers\CollectionsController@index');
 Route::get('items', 'App\Http\Controllers\ItemsController@index');
