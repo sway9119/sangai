d2a7bceb9451a829471aa26f634a4976839d4bfb
sway <40759948+sway9119@users.noreply.github.com>
2021-05-04 21:15:59 +0900

    Add NewsController

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 Route::get('collections', 'App\Http\Controllers\CollectionsController@index');
 Route::get('shop', 'App\Http\Controllers\PagesController@shop');
 Route::get('about', 'App\Http\Controllers\PagesController@about');
+Route::get('news', 'App\Http\Controllers\NewsController@index');
 Route::get('items', 'App\Http\Controllers\ItemsController@index');
