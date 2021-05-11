0f8759ec24802cacd363ce1958b93cc337d40dea
sway <40759948+sway9119@users.noreply.github.com>
2021-05-11 19:57:37 +0900

    Add AdminController
    ・管理画面用の画面とコントローラを追加
    ・Laravel/UIのセットアップ

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,20 @@ Route::get('shop', 'App\Http\Controllers\PagesController@shop');
 Route::get('about', 'App\Http\Controllers\PagesController@about');
 Route::get('news', 'App\Http\Controllers\NewsController@index');
 Route::get('items', 'App\Http\Controllers\ItemsController@index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//管理側
+Route::group(['middleware' => ['auth.admin']], function () {
+	
+	//管理側トップ
+	Route::get('/admin', 'App\Http\Controllers\admin\AdminTopController@show');
+	//ログアウト実行
+	Route::post('/admin/logout', 'App\Http\Controllers\admin\AdminLogoutController@logout');
+
+});
+
+//管理側ログイン
+Route::get('/admin/login', 'App\Http\Controllers\admin\AdminLoginController@showLoginform');
+Route::post('/admin/login', 'App\Http\Controllers\admin\AdminLoginController@login');
